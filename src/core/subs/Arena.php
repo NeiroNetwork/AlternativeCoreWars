84dbd722329d49a3b23b0480a5b535df4360c4a8
Nerahikada <hkshirorom@yahoo.co.jp>
2022-02-17 09:40:41 +0900

    とりあえずArena関係を作ってみる?

---
+++ src/core/subs/Arena.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\AlternativeCoreWars\core\subs;
+
+use pocketmine\Server;
+use pocketmine\utils\Filesystem;
+use pocketmine\world\World;
+use Webmozart\PathUtil\Path;
+
+class Arena{
+
+	public static function getArenaList() : array{
+		$result = [];
+
+		/** @var \SplFileInfo $info */
+		foreach(new \FilesystemIterator(Path::join(Server::getInstance()->getDataPath(), "worlds")) as $info){
+			try{
+				$result[] = new self($info->getBasename());
+			}catch(\InvalidArgumentException){}
+		}
+		return $result;
+	}
+
+	private Server $server;
+
+	private string $originalPath;
+	private string $temporaryPath;
+	private ?World $world = null;
+
+	private array $arenaData;
+
+	public function __construct(string $worldFolderName){
+		$this->server = Server::getInstance();
+
+		$this->originalPath = Path::join($this->server->getDataPath(), "worlds", $worldFolderName);
+
+		if(!is_dir($this->originalPath) || !file_exists(Path::join($this->originalPath, "level.dat"))){
+			throw new \InvalidArgumentException("World \"$worldFolderName\" not found");
+		}
+		if(!file_exists($json = Path::join($this->originalPath, "corepvp.json"))){
+			throw new \InvalidArgumentException("Configuration file was not found in \"$worldFolderName\"");
+		}
+
+		// TODO: できればarrayじゃなくてクラス作ってバリデーションしたい
+		$this->arenaData = json_decode(file_get_contents($json), true);
+	}
+
+	public function loadWorld() : void{
+		if($this->world !== null){
+			throw new \RuntimeException("World has already been loaded");
+		}
+
+		if(!file_exists($dir = Path::join($this->server->getDataPath(), "temporary_worlds"))){
+			mkdir($dir);
+		}
+
+		$name = bin2hex(random_bytes(8));
+		$this->temporaryPath = Path::join($dir, $name);
+
+		Filesystem::recursiveCopy($this->originalPath, $this->temporaryPath);
+
+		$this->server->getWorldManager()->loadWorld("../temporary_worlds/$name");
+		$this->world = $this->server->getWorldManager()->getWorldByName("../temporary_worlds/$name");
+	}
+
+	public function unloadWorld() : void{
+		if($this->world === null){
+			throw new \RuntimeException("World is not loaded");
+		}
+
+		$this->server->getWorldManager()->unloadWorld($this->world);
+		$this->world = null;
+
+		Filesystem::recursiveUnlink($this->temporaryPath);
+	}
+
+	public function getData() : array{
+		return $this->arenaData;
+	}
+}
---
+++ src/core/subs/ArenaData.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\AlternativeCoreWars\core\subs;
+
+class ArenaData{
+
+	private string $name;
+	private array $authors = [];
+
+	public function __construct(array $data){
+	}
+}
