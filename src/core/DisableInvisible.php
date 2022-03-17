332f2c4c223711fc56f2fb9ee27e14c0e50eeb2d
Tamikuta <58218252+Tamikuta@users.noreply.github.com>
2022-03-17 19:04:19 +0900

    サバイバルだけに反応するよ

--- src/core/DisableInvisible.php
+++ src/core/DisableInvisible.php
@@ -10,27 +10,32 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
+use pocketmine\player\GameMode;
 use pocketmine\player\Player;
 
 class DisableInvisible extends SubPluginBase implements Listener{
 
 	public function onEnable() : void{
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	public function onEntityDamageByEntity(EntityDamageByEntityEvent $event){
 		$player = $event->getEntity();
 		if($player instanceof Player){
-			if($player->getEffects()->has(VanillaEffects::INVISIBILITY())){
-				$player->getEffects()->remove(VanillaEffects::INVISIBILITY());
+			if($player->getGamemode() === GameMode::SURVIVAL()){
+				if($player->getEffects()->has(VanillaEffects::INVISIBILITY())){
+					$player->getEffects()->remove(VanillaEffects::INVISIBILITY());
+				}
 			}
 		}
 	}
 
 	public function onBlockBreak(BlockBreakEvent $event){
 		$player = $event->getPlayer();
-		if($player->getEffects()->has(VanillaEffects::INVISIBILITY())){
-			$player->getEffects()->remove(VanillaEffects::INVISIBILITY());
+		if($player->getGamemode() === GameMode::SURVIVAL()){
+			if($player->getEffects()->has(VanillaEffects::INVISIBILITY())){
+				$player->getEffects()->remove(VanillaEffects::INVISIBILITY());
+			}
 		}
 	}
 
@@ -38,8 +43,10 @@ class DisableInvisible extends SubPluginBase implements Listener{
 		if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
 			$player = $event->getEntity();
 			if($player instanceof Player){
-				if($player->getEffects()->has(VanillaEffects::INVISIBILITY())){
-					$player->getEffects()->remove(VanillaEffects::INVISIBILITY());
+				if($player->getGamemode() === GameMode::SURVIVAL()){
+					if($player->getEffects()->has(VanillaEffects::INVISIBILITY())){
+						$player->getEffects()->remove(VanillaEffects::INVISIBILITY());
+					}
 				}
 			}
 		}
