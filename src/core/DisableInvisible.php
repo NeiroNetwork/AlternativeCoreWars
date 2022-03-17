26035af6fdc722a0006434d6d0ed453d8a03b048
Tamikuta <58218252+Tamikuta@users.noreply.github.com>
2022-03-17 19:34:05 +0900

    落下ダメで消えなくしたよ

--- src/core/DisableInvisible.php
+++ src/core/DisableInvisible.php
@@ -8,7 +8,6 @@ use NeiroNetwork\AlternativeCoreWars\SubPluginBase;
 use pocketmine\entity\effect\VanillaEffects;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\event\Listener;
 use pocketmine\player\GameMode;
 use pocketmine\player\Player;
@@ -36,14 +35,5 @@ class DisableInvisible extends SubPluginBase implements Listener{
 
 	public function onBlockBreak(BlockBreakEvent $event){
 		$this->removeInvisibility($event->getPlayer());
-	}
-
-	public function onEntityDamage(EntityDamageEvent $event){
-		if($event->getCause() === EntityDamageEvent::CAUSE_FALL){
-			$player = $event->getEntity();
-			if($player instanceof Player){
-				$this->removeInvisibility($player);
-			}
-		}
 	}
 }
