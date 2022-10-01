a249a9820545ad1cd2a4e1fdc234c1b2e7523fc6
ch4rindo <103165856+ch4rindo@users.noreply.github.com>
2022-10-01 19:11:53 +0900

    versionコマンドの権限をROOT_USERから削除

--- src/core/ServerSpecificationNormalizer.php
+++ src/core/ServerSpecificationNormalizer.php
@@ -99,10 +99,14 @@ class ServerSpecificationNormalizer extends SubPluginBase implements Listener{
 	private function reduceCommandPermissions() : void{
 		$operator = PermissionManager::getInstance()->getPermission(DefaultPermissions::ROOT_OPERATOR);
 		$console = PermissionManager::getInstance()->getPermission(DefaultPermissions::ROOT_CONSOLE);
+		$user = PermissionManager::getInstance()->getPermission(DefaultPermissions::ROOT_USER);
 		$operator->removeChild(DefaultPermissionNames::COMMAND_OP_GIVE);
 		$console->addChild(DefaultPermissionNames::COMMAND_OP_GIVE, true);
 		$operator->removeChild(DefaultPermissionNames::COMMAND_DUMPMEMORY);
 		$console->addChild(DefaultPermissionNames::COMMAND_DUMPMEMORY, true);
+		$user->removeChild(DefaultPermissionNames::COMMAND_VERSION);
+		$operator->addChild(DefaultPermissionNames::COMMAND_VERSION, true);
+		$console->addChild(DefaultPermissionNames::COMMAND_VERSION, true);
 	}
 
 	private function overwriteBlocks() : void{
