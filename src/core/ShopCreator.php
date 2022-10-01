ab65375f32b583f4c6eecb40afbb5ef3f426cc12
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-01 20:25:43 +0900

    RewardEntryのsaveNameはnullableなので使用するように

--- src/core/ShopCreator.php
+++ src/core/ShopCreator.php
@@ -39,28 +39,28 @@ class ShopCreator extends SubPluginBase{
 
 	private function registerShopEntries() : void{
 		foreach([
-			(new RewardEntry("透明化のスプラッシュポーション", "不可視 2:15", uniqid("_", true)))
+			(new RewardEntry("透明化のスプラッシュポーション", "不可視 2:15"))
 				->addPrice(new MoneyPrice("money", 3000))
 				->addReward(new ItemReward(VanillaItems::INVISIBILITY_SPLASH_POTION())),
-			(new RewardEntry("耐火のスプラッシュポーション", "耐火 2:15", uniqid("_", true)))
+			(new RewardEntry("耐火のスプラッシュポーション", "耐火 2:15"))
 				->addPrice(new MoneyPrice("money", 1000))
 				->addReward(new ItemReward(VanillaItems::FIRE_RESISTANCE_SPLASH_POTION())),
-			(new RewardEntry("力のスプラッシュポーション", "力 2:15", uniqid("_", true)))
+			(new RewardEntry("力のスプラッシュポーション", "力 2:15"))
 				->addPrice(new MoneyPrice("money", 2000))
 				->addReward(new ItemReward(VanillaItems::STRENGTH_SPLASH_POTION())),
-			(new RewardEntry("俊敏のスプラッシュポーション", "スピード 2:15", uniqid("_", true)))
+			(new RewardEntry("俊敏のスプラッシュポーション", "スピード 2:15"))
 				->addPrice(new MoneyPrice("money", 1500))
 				->addReward(new ItemReward(VanillaItems::SWIFTNESS_SPLASH_POTION())),
-			(new RewardEntry("再生のスプラッシュポーション", "再生 1:30", uniqid("_", true)))
+			(new RewardEntry("再生のスプラッシュポーション", "再生 1:30"))
 				->addPrice(new MoneyPrice("money", 3000))
 				->addReward(new ItemReward(VanillaItems::REGENERATION_SPLASH_POTION())),
-			(new RewardEntry("暗視のスプラッシュポーション", "暗視 6:00", uniqid("_", true)))
+			(new RewardEntry("暗視のスプラッシュポーション", "暗視 6:00"))
 				->addPrice(new MoneyPrice("money", 500))
 				->addReward(new ItemReward(VanillaItems::LONG_NIGHT_VISION_SPLASH_POTION())),
-			(new RewardEntry("毒のスプラッシュポーション", "毒 0:33", uniqid("_", true)))
+			(new RewardEntry("毒のスプラッシュポーション", "毒 0:33"))
 				->addPrice(new MoneyPrice("money", 2000))
 				->addReward(new ItemReward(VanillaItems::POISON_SPLASH_POTION())),
-			(new RewardEntry("エンチャントのビン", "エンチャントのビン x 64", uniqid("_", true)))
+			(new RewardEntry("エンチャントのビン", "エンチャントのビン x 64"))
 				->addPrice(new MoneyPrice("money", 4000))
 				->addReward(new ItemReward(VanillaItems::EXPERIENCE_BOTTLE()->setCount(64))),
 		] as $entry) $this->shop->push($entry);
