b4b195c689697e6327f47a5b706d3afc02def781
Nerahikada <hkshirorom@yahoo.co.jp>
2022-10-02 10:26:21 +0900

    チャットを3段階に分けて処理するようにした

--- src/core/ChatRouter.php
+++ src/core/ChatRouter.php
@@ -5,51 +5,61 @@ declare(strict_types=1);
 namespace NeiroNetwork\AlternativeCoreWars\core;
 
 use NeiroNetwork\AlternativeCoreWars\constants\BroadcastChannels;
-use NeiroNetwork\AlternativeCoreWars\constants\Teams;
-use NeiroNetwork\AlternativeCoreWars\event\GameCleanupEvent;
 use NeiroNetwork\AlternativeCoreWars\SubPluginBase;
 use pocketmine\console\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
 class ChatRouter extends SubPluginBase implements Listener{
 
-	private const TAG_ALL = TextFormat::RED . "[ALL]" . TextFormat::RESET;
-	private const TAG_TEAM = TextFormat::GOLD . "[TEAM]" . TextFormat::RESET;
+	private const FLAG_NORMAL = 0x00;
+	private const FLAG_ALL = 0x01;
+
+	private array $chatFlag = [];
 
 	protected function onEnable() : void{
+		// FIXME: ConsoleCommandSender を新しく作っていいのか分からない (コンソールは1つなのに) (挙動的には問題ない)
+		$console = new ConsoleCommandSender($this->getServer(), $this->getServer()->getLanguage());
+		$this->getServer()->subscribeToBroadcastChannel(BroadcastChannels::RED, $console);
+		$this->getServer()->subscribeToBroadcastChannel(BroadcastChannels::BLUE, $console);
+
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->initChatRouter();
 	}
 
-	private function initChatRouter(){
-		$this->getServer()->subscribeToBroadcastChannel(BroadcastChannels::fromTeam(Teams::RED), new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage()));
-		$this->getServer()->subscribeToBroadcastChannel(BroadcastChannels::fromTeam(Teams::BLUE), new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage()));
-	}
-
-	public function onChat(PlayerChatEvent $event) : void{
-		$player = $event->getPlayer();
-
-		$team = TeamReferee::getTeam($player);
-		if(!is_null($team)){
-			$message = $event->getMessage();
-
-			if(str_starts_with($event->getMessage(), "!")){
-				$message = substr($message, 1);
-				$message = self::TAG_ALL . " " . $message;
-				$event->setMessage($message);
-			}else{
-				$message = self::TAG_TEAM . " " . $message;
-				$recipients = $this->getServer()->getBroadcastChannelSubscribers(BroadcastChannels::fromTeam($team));
-				$event->setRecipients($recipients);
-				$event->setMessage($message);
-			}
+	/**
+	 * @priority LOWEST
+	 */
+	public function onChat1(PlayerChatEvent $event) : void{
+		$message = $event->getMessage();
+		if(str_starts_with($message, "!")){
+			$this->chatFlag[spl_object_id($event)] = self::FLAG_ALL;
+			$event->setMessage(substr($message, 1));
+		}else{
+			$this->chatFlag[spl_object_id($event)] = self::FLAG_NORMAL;
 		}
 	}
 
-	public function onEndGame(GameCleanupEvent $event){
-		$this->initChatRouter();
+	public function onChat2(PlayerChatEvent $event) : void{
+		if(!Game::getInstance()->isRunning() || is_null($team = TeamReferee::getTeam($event->getPlayer()))) return;
+
+		$message = $event->getMessage();
+		switch($this->chatFlag[spl_object_id($event)]){
+			case self::FLAG_NORMAL:
+				$event->setMessage(TextFormat::GRAY . $message);
+				$event->setRecipients($this->getServer()->getBroadcastChannelSubscribers(BroadcastChannels::fromTeam($team)));
+				break;
+			case self::FLAG_ALL:
+				// NOOP
+				break;
+		}
+	}
+
+	/**
+	 * @handleCancelled
+	 * @priority MONITOR
+	 */
+	public function onChat3(PlayerChatEvent $event) : void{
+		unset($this->chatFlag[spl_object_id($event)]);
 	}
 }
