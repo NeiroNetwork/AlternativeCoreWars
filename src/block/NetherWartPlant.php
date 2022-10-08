907b1cb0d2373236abed2118f56df9f380b409de
Nerahikada <hkshirorom@yahoo.co.jp>
2022-10-08 19:39:42 +0900

    ポーションの材料のドロップ率を変更

--- src/block/NetherWartPlant.php
+++ src/block/NetherWartPlant.php
@@ -11,8 +11,9 @@ class NetherWartPlant extends \pocketmine\block\NetherWartPlant{
 
 	public function getDropsForCompatibleTool(Item $item) : array{
 		$drops[] = $this->asItem()->setCount(($isMature = $this->age === 3) ? mt_rand(0, 2) : mt_rand(-2, 1));
-		if(mt_rand(1, $isMature ? 16 : 32) === 1) $drops[] = VanillaItems::FERMENTED_SPIDER_EYE();
-		if(mt_rand(1, $isMature ? 25 : 50) === 1) $drops[] = VanillaItems::GUNPOWDER();
+		if(mt_rand(1, $isMature ? 4 : 8) === 1) $drops[] = VanillaItems::FERMENTED_SPIDER_EYE();
+		if(mt_rand(1, $isMature ? 7 : 14) === 1) $drops[] = VanillaItems::GHAST_TEAR();
+		if(mt_rand(1, $isMature ? 8 : 16) === 1) $drops[] = VanillaItems::GUNPOWDER();
 
 		if(count($drops) > 1 && !$drops[0]->isNull()) $drops[0]->pop();
 
