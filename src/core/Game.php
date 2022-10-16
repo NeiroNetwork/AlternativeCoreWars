58e2d51dd5706b0e3dcca95d4ce2e69b33ce4ac0
banira4649 <banira.game@outlook.jp>
2022-10-17 00:10:44 +0900

    PlayerSkillActivateEventをKitEventBrokerでリッスンするように

--- src/core/Game.php
+++ src/core/Game.php
@@ -27,7 +27,6 @@ use NeiroNetwork\ExperimentalFeatures\feature\v1_2\entity\FireworksRocket;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_2\item\FireworkColor;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_2\item\Fireworks;
 use NeiroNetwork\ExperimentalFeatures\feature\v1_2\item\FireworkType;
-use NeiroNetwork\Kits\event\player\PlayerSkillActivateEvent;
 use pocketmine\block\DiamondOre;
 use pocketmine\block\VanillaBlocks;
 use pocketmine\entity\effect\EffectInstance;
@@ -414,12 +413,6 @@ class Game extends SubPluginBase implements Listener{
 			));
 		}elseif($from === $this->getWorld()){
 			$player->getNetworkSession()->sendDataPacket(BossEventPacket::hide($player->getId()));
-		}
-	}
-
-	public function onPlayerSkillActivate(PlayerSkillActivateEvent $event) : void{
-		if(!$this->isRunning()){
-			$event->cancel();
 		}
 	}
 }
